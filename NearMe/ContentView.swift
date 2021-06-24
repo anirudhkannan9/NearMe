83682a4d36f6eb63ca8a5f0c4aabb660ae61218c
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-06-24 14:39:18 -0700

    Added "Cancel" button when searching; added animation to "Cancel" button and search bar itself (slide in/slide out). Added functionality to Cancel button (clears searched text, etc.). Extracted SearchBar into its own View/component. Tested out Dark Mode (doesn't work as well in Dark Mode).

--- NearMe/ContentView.swift
+++ NearMe/ContentView.swift
@@ -21,57 +21,67 @@ struct ContentView: View {
                 Text("NearMe")
                     .font(.largeTitle)
                     .foregroundColor(Color.white)
-                HStack {
-                TextField("Find a product near you", text: $searchText)
-                    .padding(.leading, 24)
-                }
-                    .padding()
-                    .background(Color(.systemGray6))
-                    .cornerRadius(16)
-                    .padding()
-                    .onTapGesture(perform: {
-                        isSearching = true
-                        
-                    })
-                    .overlay(
-                        HStack{
-                            Image(systemName: "magnifyingglass")
-                            Spacer()
-                            
-                            if isSearching {
-                                Button(action: { searchText = ""}, label: {
-                                    Image(systemName: "xmark.circle")
-                                })
-                            }
-                        }
-                        .padding(.horizontal, 32)
-                        .foregroundColor(.black)
-                    )
-                //}
+                
+                SearchBar(searchText: $searchText, isSearching: $isSearching)
             }
         }
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+            .colorScheme(.dark)
+    }
+}
+
+struct SearchBar: View {
+    
+    @Binding var searchText: String
+    @Binding var isSearching: Bool
+    
+    var body: some View {
+        HStack {
+            HStack {
+            TextField("Find a product near you", text: $searchText)
+                .padding(.leading, 24)
+            }
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(16)
+                .padding()
+                .onTapGesture(perform: {
+                    isSearching = true
+                    
+                })
+                .overlay(
+                    HStack{
+                        Image(systemName: "magnifyingglass")
+                        Spacer()
+                        
+                        if isSearching {
+                            Button(action: { searchText = ""}, label: {
+                                Image(systemName: "xmark.circle")
+                            })
+                        }
+                    }
+                    .padding(.horizontal, 32)
+                    .foregroundColor(.black)
+                ).transition(.move(edge: .trailing))
+                 .animation(.easeIn)
+            if isSearching {
+                Button(action: {
+                    isSearching = false
+                    searchText = ""
+                }, label: {
+                    Text("Cancel")
+                        .padding(.trailing, 15)
+                        //.padding(.leading, 10)
+                        .foregroundColor(.black)
+                })
+                .transition(.move(edge: .trailing))
+                .animation(.easeIn)
+            }
+        }
     }
 }
